326d01d20e9e9b2cc09999f3430f045eaef4cfdc
Marina <m.chaychits@gmail.com>
2025-05-04 02:19:19 +0300

    hw

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,39 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+/*
+* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+* Полученное булево значение сохраните в переменной isAccess.
+
+* Условия доступа:
+* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
+* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+*/
+
+// тестовые данные (значения можно менять)
+
+const isAdmin = "Да"
+const isVerifiedUser = "Да"
+const hasSpecialPermission = "Да"
+const hasTemporaryPass = "Да"
 
 let isAccess
 
+let user = prompt("What is your name?");
+let verification = prompt("Do you have verification?");
+let permission = prompt("Do you have permissions?");
+let pass = prompt("Do you have pass?");
+
+if((user === isAdmin || verification === isVerifiedUser) && permission === hasSpecialPermission && pass === hasTemporaryPass ){
+    isAccess = true;
+    alert("ok")
+}
+else{
+    isAccess = false;
+    alert("Доступ отклонен")}
+
+
 // your code
+
+
+
