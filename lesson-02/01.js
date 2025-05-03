64ca131045cbf11a174ba64b0cbf2b701327e316
Marina <m.chaychits@gmail.com>
2025-05-04 02:45:13 +0300

    hw4

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,45 +1,18 @@
-/*
-* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
-* Полученное булево значение сохраните в переменной isAccess.
-
-* Условия доступа:
-* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
-* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
-*/
-
-// тестовые данные (значения можно менять)
-/*
-* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
-* Полученное булево значение сохраните в переменной isAccess.
-
-* Условия доступа:
-* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
-* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
-*/
-
-// тестовые данные (значения можно менять)
-
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = "Да"
+const isVerifiedUser = "Да"
+const hasSpecialPermission = "Да"
+const hasTemporaryPass = "Да"
 
 let isAccess
 
-let user = isAdmin
-let verification = isVerifiedUser
-let permission = hasSpecialPermission
-let pass = hasTemporaryPass
+let user = prompt("What is your name?");
+let verification = prompt("Do you have verification?");
+let permission = prompt("Do you have permissions?");
+let pass = prompt("Do you have pass?");
 
-if((user === false || verification === true) && permission === true && pass === false ){
+if((user === isAdmin || verification === isVerifiedUser) && (permission === hasSpecialPermission || pass === hasTemporaryPass )){
     isAccess = true;
 }
 else{
     isAccess = false;
-    }
-
-
-// your code
-
-
-
+}
