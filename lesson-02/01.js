9b9c1065ad5043b32f635633115df26fd1e9b854
Marina <m.chaychits@gmail.com>
2025-05-04 10:07:16 +0300

    hw

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,7 +1,7 @@
-const isAdmin = "Да"
-const isVerifiedUser = "Да"
-const hasSpecialPermission = "Да"
-const hasTemporaryPass = "Да"
+const isAdmin = false
+const isVerifiedUser = true
+const hasSpecialPermission = true
+const hasTemporaryPass = false
 
 let isAccess
 
@@ -10,11 +10,11 @@ let user = prompt("What is your name?");
 let permission = prompt("Do you have permissions?");
 // let pass = prompt("Do you have pass?");
 
-if((user === isAdmin && (permission === hasSpecialPermission || permission === hasTemporaryPass )) || (user === isVerifiedUser && (permission === hasSpecialPermission || permission === hasTemporaryPass ))){
-    isAccess = true;
+if((user === !isAdmin && (permission === hasSpecialPermission || permission === !hasTemporaryPass )) || (user === isVerifiedUser && (permission === hasSpecialPermission || !permission === hasTemporaryPass ))){
+    isAccess = true
 }
 else{
-    isAccess = false;
+    isAccess = false
 }
 
 
