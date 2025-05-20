f8de58738e9d4b77379a17b248317d06c5913a94
Marina <m.chaychits@gmail.com>
2025-05-20 16:50:32 +0300

    hw

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,10 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+const array = [1, 2, 3, 4, 5]
+
+function includesElement(array, element) {
+    return array.includes(element);
+
+}
+console.log(includesElement(array, 0));
